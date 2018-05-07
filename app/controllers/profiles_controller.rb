e959b36beb5df4ead1b10d0f519325c8fe09b96e
ChristopherForrest <Christopherforrest92@gmail.com>
2018-05-07 18:34:31 +1000

    fix carrierwave

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -47,7 +47,7 @@ class ProfilesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def game_params
-      params.require(:profile).permit(:name, :age, :location, :bio)
+      params.require(:profile).permit(:name, :age, :location, :bio, :profilepicture, :favouritegames)
     end
 
 end
