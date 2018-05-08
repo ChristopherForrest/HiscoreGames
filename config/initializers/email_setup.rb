25b8bde4e0f4017297f3e02bd3d8493de15175ac
ChristopherForrest <Christopherforrest92@gmail.com>
2018-05-08 14:34:48 +1000

    fixed mailed

--- config/initializers/email_setup.rb
+++ config/initializers/email_setup.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'your-app-name.herokuapp.com',
+  domain:               'https://hiscore-games.herokuapp.com/',
   user_name:            ENV["gmail_username"],
   password:             ENV["gmail_password"],
   authentication:       'plain',
