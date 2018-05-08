1d35f284144295d1a268d325af798ca4260e8741
ChristopherForrest <Christopherforrest92@gmail.com>
2018-05-08 18:05:40 +1000

    sds

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -66,6 +66,9 @@ class GamesController < ApplicationController
     end
   end
 
+  def purchase
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_game
