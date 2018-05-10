08e30ef3f786294f5d3ca6e72479faf5ab364755
ChristopherForrest <Christopherforrest92@gmail.com>
2018-05-10 16:58:48 +1000

    sds

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,19 @@
 class MessagesController < ApplicationController
     before_action :set_conversation
-
+ 
 
 
     def create
+        unless params[:body].empty?
         receipt = current_user.reply_to_conversation(@conversation, params[:body])
         redirect_to conversation_path(receipt.conversation)
+        
     end
+    end
+
+
+        
+    
 
 
     private
@@ -15,4 +22,10 @@ class MessagesController < ApplicationController
         @conversation = current_user.mailbox.conversations.find(params[:conversation_id])
     end
 
-end
+
+
+
+
+
+end 
+
