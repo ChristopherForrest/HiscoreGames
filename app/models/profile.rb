49670df7d9cd37ce46d95ee3d4beadc468cda4b6
ChristopherForrest <Christopherforrest92@gmail.com>
2018-05-10 11:18:23 +1000

    sds

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,5 @@
 class Profile < ApplicationRecord
     mount_uploader :profilepicture, ImageUploader
     validates :user_id, uniqueness: true
-    
     belongs_to :user
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,16 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe 'Game', type: :model do
-
-        it 'ensures title is in' do
+        
+        # no details
+        it 'ensures invalid postings dont save' do
             game = Game.new
             expect(game).to_not be_valid
         end 
 
-
-        it 'ensures valid' do
+        # correct details
+        it 'ensures validations are correct' do
             user = User.create(email: "sd@sds", password:"sdssds")
             game = Game.new(title:"sds", price:9.90, description:"sds", user_id: user.id)
             expect(game).to be_valid
         end
+
+        # duplicated users
+        it 'ensures user is unique' do
+            user = User.create(email: "sd@sd", password:"sdssds")
+            user = User.create(email: "sd@sd", password:"sdssds")
+            expect(user).to_not be_valid
+        end
+        
+
 end
