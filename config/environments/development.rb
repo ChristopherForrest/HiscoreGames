af5286438de7d236c606be82b65e96afa9ca998a
ChristopherForrest <Christopherforrest92@gmail.com>
2018-05-10 18:25:02 +1000

    fix email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { host: 'https://hiscore-au.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://hiscore-games.herokuapp.com/' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
